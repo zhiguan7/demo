055f148b07ebaf4d658550495f2d6f4b7ea761c4
0phone <zhiguan7@gmail.com>
2020-12-09 18:56:37 +0800

    2

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.pojo.Dog;
+import com.example.demo.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	Dog dog;
+
+	@Autowired
+	Person person;
+
 	@Test
 	void contextLoads() {
+
+		System.out.println(person);
 	}
 
 }
